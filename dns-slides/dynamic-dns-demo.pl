2a4c1c10c5e9ac06cde92e5143fbf3ca64a08410
chenryn <rao.chenlin@gmail.com>
2013-03-21 10:52:34 +0800

    delete \s*

--- dns-slides/dynamic-dns-demo.pl
+++ dns-slides/dynamic-dns-demo.pl
@@ -6,6 +6,7 @@ use 5.014;
 use Net::IP::Match::Regexp qw/create_iprange_regexp match_ip/;
 use Stanford::DNS;
 use Stanford::DNSserver;
+
 my $hostmaster = 'admin.renren-inc.com';
 my $hostname   = '127.0.0.1';
 my $soa        = rr_SOA($hostname, $hostmaster, time(), 3600, 1800, 86400, 0);
@@ -15,14 +16,19 @@ my $ns         = Stanford::DNSserver->new( listen_on => [ $hostname ],
 					   daemon    => 'no',
 					 );
 my $host       = 'chenlin.rao.som.renren-inc.com';
+
 my $regexp     = create_iprange_regexp({ '127.0.0.1/32' => 'localhost',
 	                                 '10.0.0.0/8'   => 'ethernet',
 				      });
 my $iplist     = { $host => { ethernet => '10.10.10.10', localhost => '12.7.0.10'}, };
+
 $ns->add_static( $host, T_SOA, $soa );
 $ns->add_static( $host, T_NS, rr_NS($hostmaster) );
+
 $ns->add_dynamic( $host => \&dyn_req );
+
 $ns->answer_queries();
+
 sub dyn_req {
     my ($domain,$residual,$qtype,$qclass,$dm,$from) = @_;
     my $ttl = 3600;
@@ -42,6 +48,7 @@ sub dyn_req {
         $dm->{rcode} = NXDOMAIN;
     };
 };
+
 sub dyn_match {
     my ( $domain, $from ) = @_;
     my $area = match_ip($from, $regexp);
--- dns-slides/netdns.pl
+++ dns-slides/netdns.pl
@@ -5,16 +5,21 @@ use warnings;
 use Data::Dumper;
 use autodie;
 use Net::DNS::Nameserver;
+
 my $ns = new Net::DNS::Nameserver( LocalPort    => 5353,
                                    ReplyHandler => \&reply_handler,
                                    Verbose      => 1
                                  );
+
 $ns->main_loop;
+
 sub reply_handler {
     my ($qname, $qclass, $qtype, $peerhost, $query, $conn) = @_;
     my ($rcode, @ans, @auth, @add);
+
     $query->print;
 #    print Dumper $conn;
+
     if ($qtype eq "A" && $qname =~ m/foobar.com$/ ) {
             my ($ttl, $rdata) = (3600, "10.1.2.3");
             my $rr = new Net::DNS::RR("$qname $ttl $qclass $qtype $rdata");
@@ -29,6 +34,7 @@ sub reply_handler {
     }else{
             $rcode = "NXDOMAIN";
     }
+
     # mark the answer as authoritive by setting the 'aa' flag
     return ($rcode, \@ans, \@auth, \@add, { aa => 1 });
 }
