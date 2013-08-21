e2eec07522c33f69fb33b2acbe646af65a4bcfbd
chenryn <rao.chenlin@gmail.com>
2013-08-21 16:54:48 +0800

    new image

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 msg=$1
 if [[ "x$msg" == "x" ]];then
-    msg = "new post"
+    msg="new post"
 fi
 for i in `find ./_site/ -type f -name '*.html'`;do sed -i '/^\s*$/d' $i;done
 git add .
